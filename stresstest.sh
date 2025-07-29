355c5c01359aa0fd9261a7e8adc123b03eb980cc
Stephen Ng <stephen5.ng@gmail.com>
2025-07-28 23:21:37 -0700

    fix stress test

--- stresstest.sh
+++ stresstest.sh
@@ -6,6 +6,7 @@ trap "kill 0" EXIT
 duration="${1:-0.1}"
 
 
-python fake_tile_sequences.py --duration $duration --player 2 &
+#python fake_tile_sequences.py --duration $duration --player 2 &
+python fake_tile_sequences.py --duration $duration --player 1 &
 
 ./runpygame.sh
