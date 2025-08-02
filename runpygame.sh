c652d3f76f2c0bfd765d27a93ae0b06c9540ef4b
Stephen Ng <stephen5.ng@gmail.com>
2025-08-02 09:01:57 -0700

    fix script so it can be called by test

--- runpygame.sh
+++ runpygame.sh
@@ -2,11 +2,21 @@
 
 . cube_env/bin/activate
 export PYTHONPATH=../easing-functions:../rpi-rgb-led-matrix/bindings/python:$PYTHONPATH
-trap "kill 0" EXIT
 mqtt_server=${MQTT_SERVER:-localhost}
+mosquitto_pid=""
 if ! nc -zv $mqtt_server 1883 > /dev/null 2>&1; then
   /opt/homebrew/opt/mosquitto/sbin/mosquitto -c /opt/homebrew/etc/mosquitto/mosquitto.conf &
+  mosquitto_pid=$!
 fi
+
+# Clean up function that only kills the mosquitto process we started
+cleanup() {
+  if [ -n "$mosquitto_pid" ]; then
+    kill $mosquitto_pid 2>/dev/null || true
+  fi
+}
+
+trap cleanup EXIT
 
 
 #python -X dev -X tracemalloc=5 ./main.py "$@"
