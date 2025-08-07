1090363624c16234d27d2ead38c3333b463c803b
Stephen Ng <stephen5.ng@gmail.com>
2025-08-07 06:26:29 -0700

    add run_functional_tests.sh

---
+++ run_functional_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+tests=(
+    gamepad
+    sng
+    stress_0.01
+    stress_0.1
+)
+
+for test in "${tests[@]}"; do
+    echo "====================================================================="
+    echo "Running test: $test"
+    echo "====================================================================="
+    ./functional_test.py replay "$test"
+done
+
+echo "All functional tests passed!"
+
