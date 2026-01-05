96825e5d2ebe6074b51c480cd4b0ec831ebfd8bf
Stephen Ng <stephen5.ng@gmail.com>
2026-01-04 17:48:17 -0800

    update sleep and wake

--- tools/sleep.sh
+++ tools/sleep.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
 
-while IFS= read -r line; do
-  mosquitto_pub -h $MQTT_SERVER -t "cube/$line/sleep" -m "1" --retain
-#  mosquitto_pub -h localhost -t "cube/$line/sleep" -m "1" --retain
-done < "cube_ids.txt"
+mosquitto_pub -h $MQTT_SERVER -t "cube/sleep" -m "1" --retain
--- tools/wake.sh
+++ tools/wake.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
 
-while IFS= read -r line; do
-  mosquitto_pub -h $MQTT_SERVER -t "cube/$line/sleep" -m "0" --retain
-#  mosquitto_pub -h localhost -t "cube/$line/sleep" -m "0" --retain
-done < "cube_ids.txt"
+mosquitto_pub -h $MQTT_SERVER -t "cube/sleep" -m "" --retain
